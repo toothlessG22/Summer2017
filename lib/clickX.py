2812808423f7ca2baf0369e3802941995a35d3a7
toothless <toothlessG22@gmail.com>
2017-05-26 15:29:29 -0400

    make sure pos exists

--- lib/clickX.py
+++ lib/clickX.py
@@ -10,22 +10,25 @@ else:
 
 def clickX():
     while openCVLocate.locateCenter(imgpath + '/X.PNG') is None:
-        if(openCVLocate.locateCenter(imgpath + '/OK.PNG') is not None):
+        pos = openCVLocate.locateCenter(imgpath + '/OK.PNG')
+        if pos is not None:
             print('see OK')
-            pos = openCVLocate.locateCenter(imgpath + '/OK.PNG')
             pyautogui.click(pos, duration=.6)
             pyautogui.moveTo(x=pos[0], y=pos[1]+40)
 
+
     while openCVLocate.locateCenter(imgpath + '/X.PNG') is not None:
         pos = openCVLocate.locateCenter(imgpath + '/OK.PNG')
-        pyautogui.click(pos, duration=.6)
-        pyautogui.moveTo(x=pos[0], y=pos[1] + 40)
+        if pos is not None:
+            pyautogui.click(pos, duration=.6)
+            pyautogui.moveTo(x=pos[0], y=pos[1] + 40)
         time.sleep(2)
         pyautogui.click(openCVLocate.locateCenter(imgpath + '/X.PNG'))
         time.sleep(2)
         pos = openCVLocate.locateCenter(imgpath + '/OK.PNG')
-        pyautogui.click(pos, duration=.6)
-        pyautogui.moveTo(x=pos[0], y=pos[1] + 40)
+        if pos is not None:
+            pyautogui.click(pos, duration=.6)
+            pyautogui.moveTo(x=pos[0], y=pos[1] + 40)
 
 if __name__ == "__main__":
     clickX()
