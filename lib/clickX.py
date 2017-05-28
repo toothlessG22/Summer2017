c31f2d65fdf209fe7c1d4f30b634c24dc2351e84
toothless <toothlessG22@gmail.com>
2017-05-28 13:28:31 -0400

    play change

--- lib/clickX.py
+++ lib/clickX.py
@@ -10,23 +10,23 @@ else:
 
 def clickX():
     while openCVLocate.locateCenter(imgpath + '/X.PNG', thold=.95) is None:
-        pos = openCVLocate.locateCenter(imgpath + '/OK.PNG')
+        pos = openCVLocate.locateCenter(imgpath + '/OK.PNG', thold=.65)
         if pos is not None:
             print('see OK')
             pyautogui.click(pos, duration=.6)
             pyautogui.moveTo(x=pos[0], y=pos[1]+40)
         time.sleep(1)
 
-
+    time.sleep(10)
     while openCVLocate.locateCenter(imgpath + '/X.PNG', thold=.95) is not None:
-        pos = openCVLocate.locateCenter(imgpath + '/OK.PNG')
+        pos = openCVLocate.locateCenter(imgpath + '/OK.PNG', thold=.65)
         if pos is not None:
             pyautogui.click(pos, duration=.6)
             pyautogui.moveTo(x=pos[0], y=pos[1] + 40)
         time.sleep(2)
         pyautogui.click(openCVLocate.locateCenter(imgpath + '/X.PNG'))
         time.sleep(2)
-        pos = openCVLocate.locateCenter(imgpath + '/OK.PNG')
+        pos = openCVLocate.locateCenter(imgpath + '/OK.PNG', thold=.65)
         if pos is not None:
             pyautogui.click(pos, duration=.6)
             pyautogui.moveTo(x=pos[0], y=pos[1] + 40)
